5869b0ed3c9322b813b19349cebc10fbf04f0586
jeffry <jeffryacai@gmail.com>
2024-02-23 23:26:23 +0700

    revision query logic result table

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -17,27 +17,34 @@ class ResultController extends Controller
     public function index()
     {
         $results = DB::table('results')
-        ->select('users.id',
-        'users.name',
-        'users.email',
-        'users_details.education_level',
-        'users_details.phone_number',
-        'users_details.school_name',
-        'users_details.occupation_desc',
-        'results.types_id',
-        'results.score',
-        'results.start_time',
-        'results.end_time',
-        'results.difference',
-        'results.created_at',
-        'events.title as event_title',
-        'types.type_name as type_title')
+        ->select(
+            'users.id as user_id',
+            'users.name',
+            'users.email',
+            'users_details.education_level',
+            'users_details.phone_number',
+            'users_details.school_name',
+            'users_details.occupation_desc',
+            'results.types_id',
+            'results.score',
+            'results.start_time',
+            'results.end_time',
+            'results.difference',
+            'results.created_at',
+            'events.title as event_title',
+            'types.type_name as type_title'
+        )
         ->join('users', 'users.id', '=', 'results.user_id')
-        ->join('users_details', 'users_details.id', '=', 'results.user_id')
+        ->join('users_details', 'users_details.user_id', '=', 'results.user_id')
         ->join('events', 'events.id', '=', 'results.event_id')
-        ->join('types',  'types.id', '=', 'results.types_id')
+        ->join('types', 'types.id', '=', 'results.types_id')
         ->orderBy('results.created_at', 'DESC')
         ->get();
+
+        // $results = DB::table('results')
+        // ->get();
+
+        // dd($results);
 
         return view('result', compact('results'));
     }
@@ -65,13 +72,13 @@ class ResultController extends Controller
         'events.title as event_title',
         'types.type_name as type_title')
         ->join('users', 'users.id', '=', 'results.user_id')
-        ->join('users_details', 'users_details.id', '=', 'results.user_id')
+        ->join('users_details', 'users_details.user_id', '=', 'results.user_id')
         ->join('events', 'events.id', '=', 'results.event_id')
         ->join('types',  'types.id', '=', 'results.types_id')
         ->orderBy('results.created_at', 'DESC')
         ->get();
     $mpdf->WriteHTML(view('components.table.resultTable', ['results' => $results]));
-    $mpdf->Output('download-pdf-result','D');
+    $mpdf->Output('download-pdf-result.pdf','D');
     }
 
     /**
